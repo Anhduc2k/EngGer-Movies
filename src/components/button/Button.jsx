623f9364e99a04d40cf2a200b320dc2c67422b3e
AnhDuc2k <khungkhip@gmail.com>
2021-10-12 16:20:14 +0700

    make button

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = props => {
+  return (
+    <button
+      className={`btn ${props.className}`}
+      onCLick={props.onCLick ? () => props.onCLick() : null}
+    >
+      {props.children}
+    </button>
+  )
+}
+const OutlineButton = props => {
+  return (
+    <Button
+      className={`btn-outline ${props.className}`}
+      onCLick={props.onCLick ? () => props.onCLick() : null}
+    >
+      {props.children}
+    </Button>
+  )
+}
+Button.propTypes = {
+  onCLick: PropTypes.func
+}
+
+export default Button
