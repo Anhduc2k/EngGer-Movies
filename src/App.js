bf6b71aa6e54d1cbc607523b6c2931b5098e55b9
AnhDuc2k <khungkhip@gmail.com>
2021-10-12 10:43:26 +0700

    make header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import 'swiper/swiper.min.css'
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
 import './App.scss'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './components/header/Header'
+import Routes from './config/Routes'
+import Footer from './components/footer/Footer'
 
 function App() {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Route
+        render={props => (
+          <>
+            <Header {...props} />
+            <Routes />
+            <Footer />
+          </>
+        )}
+      />
+    </BrowserRouter>
+  )
 }
 
 export default App
