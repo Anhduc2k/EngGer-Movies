6eb4eb7446359dee9797a45b0173417d7668a1f7
AnhDuc2k <khungkhip@gmail.com>
2021-10-13 11:33:23 +0700

    make footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
-
+import './footer.scss'
+import bg from '../../assets/footer-bg.jpg'
+import logo from '../../assets/tmovie.png'
+import { Link } from 'react-router-dom'
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className="logo">
+            <img src={logo} alt="" />
+            <Link to="/">EngGer Movies</Link>
+          </div>
+        </div>
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link
+              to={{ pathname: 'https://www.facebook.com/duclapulga/' }}
+              target="_blank"
+              style={{ color: 'rgb(90, 146, 197)' }}
+            >
+              Contact us
+            </Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">About us</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Policy</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">You must watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">Top IMDB</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
